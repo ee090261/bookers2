14eb573dc9b668e7db487df9a3d3c458d471d749
ee090261 <gear.tbf09g.winf@gmail.com>
2020-09-14 14:37:50 +0900

    modify feedback

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about'
-  resources :books, only: [:index, :show, :edit, :update, :destory, :create]
+  resources :books, only: [:index, :show, :edit, :update, :destroy, :create]
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
